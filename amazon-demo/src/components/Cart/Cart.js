af23327acbc4530c30f9da4e430acb5f144b9084
Kumu <kumusadia02@gmail.com>
2020-03-18 04:24:44 +0600

    amazon clone

--- amazon-demo/src/components/Cart/Cart.js
+++ amazon-demo/src/components/Cart/Cart.js
@@ -1,21 +1,24 @@
 import React from 'react';
 
+
 const Cart = (props) => {
     const cart = props.cart;
-    const totalPrice = cart.reduce((total, prd) => total + prd.price, 0);
-    let shipping = 12.99;
-    if(totalPrice > 35){
-        shipping = 0;
-    }
-    else if(totalPrice > 15){
-        shipping=4.99;
+    //const totalPrice = cart.reduce((total, prd) => total + prd.price, 0);
+    let total = 0;
+    for(let i = 0 ; i<cart.length; i++){
+        const product = cart[i];
+        total = total + product.price*product.quantity;
+        debugger;
     }
     return (
         <div>
             <h4>Order summery</h4>
             <p>Items Ordered {cart.length}</p>
-            <p>Shipping cast {shipping}</p>
-            <p>Total price {totalPrice+shipping}</p>
+            <p>Total price {total}</p>
+            <br/>
+            {
+                props.children
+            }
         </div>
     );
 };
--- amazon-demo/src/components/Product/Product.js
+++ amazon-demo/src/components/Product/Product.js
@@ -2,23 +2,24 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee ,faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
+import {Link} from 'react-router-dom';
 const Product = (props) => {
-    //console.log(props);
-    const {img, name, seller,price,stock} = props.product;
+    console.log(props);
+    const {img, name, seller,price,stock,key} = props.product;
     return (
         <div className = "product">
            <div>
                 <img src={img} alt=""/>
            </div>
-           <div className="product-name"> 
-               <h4>{name}</h4>
+           <div className="product-spaceing"> 
+               <h4 className="product-name"><Link to={"/product/"+key}>{name}</Link></h4>
                <p><small>by: {seller}</small></p>
                <p><small>$ {price}</small></p>
                <p><small>Only {stock} left in stock - Order soon </small></p>
-               <button 
+               {props.showAddToCart === true && <button 
                className="main-button"
                onClick = {() => props.handleAddProduct(props.product)}>
-               <FontAwesomeIcon icon={faShoppingCart} />Add to cart</button>
+               <FontAwesomeIcon icon={faShoppingCart} />Add to cart</button>}
            </div>
         </div>
     );
