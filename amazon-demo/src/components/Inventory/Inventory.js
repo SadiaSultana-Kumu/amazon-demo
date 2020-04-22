e0e46624bea5fd96616a9c9a4afe55cf83cc98e9
Kumu <kumusadia02@gmail.com>
2020-04-22 23:28:15 +0600

    amazon-clone

--- amazon-demo/src/components/Inventory/Inventory.js
+++ amazon-demo/src/components/Inventory/Inventory.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const Inventory = () => {
+    const handleAddInventory = () =>{
+        
+    }
     return (
         <div>
-            <h1>Order History coming soon...</h1>
+            <h1>Add Inventory to Sell more...</h1>
+            <button onClick={handleAddInventory} >Add Inventor</button>
         </div>
     );
 };
